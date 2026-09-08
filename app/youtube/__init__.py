8f831e59f058d6590e75043c2a1a96e3221d7848
Ansar Sakeen <ansarsakeen75@gmail.com>
2026-09-08 11:36:42 +0530

    Update __init__.py

--- app/youtube/__init__.py
+++ app/youtube/__init__.py
@@ -1,1 +1,20 @@
+import os
+from flask import Flask, render_template
+from youtube.routes import youtube_bp
 
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config["GEMINI_API_KEY"] = os.getenv("GEMINI_API_KEY")
+
+    app.register_blueprint(
+        youtube_bp,
+        url_prefix="/youtube"
+    )
+
+    @app.route("/")
+    def home():
+        return render_template("index.html")
+
+    return app
