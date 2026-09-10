69eab7d1987c90a6d64591fd4b7b34de36ed6dd3
Tjayyy2112 <chocogaming859@gmail.com>
2026-09-10 14:18:12 +0530

    Fix imports and update KEYWORDS structure

--- app/gmail/gmail_write.py
+++ app/gmail/gmail_write.py
@@ -1,9 +1,12 @@
-import os
+import os 
 import re
-import urllib.parse
+import urlib.parse
 
-CLIENT_EMAIL =os.getenv("CLIENT_EMAIL","")
+CLIENT_EMAIL = os.getenv("CLIENT_EMAIL","")
 
-KEYWORDS = {
-  
-}
+KEYWORDS = (
+   "gmail", "email", "e-mail", "mail",
+    "write an email", "send an email", "draft an email",
+    "compose an email", "write mail", "send mail", "draft mail",
+    "compose mail"
+)
